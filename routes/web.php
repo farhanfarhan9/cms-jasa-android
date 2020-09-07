397f61efe6b36aa8c48863f75f4a58277976e165
Farhan <faarhan7@gmail.com>
2020-09-07 14:58:44 +0700

    template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('frontend.index');
 });
+Route::get('/s', function () {
+    return view('backend.index');
+});
